1dd83f357ef9d53ad5f0a81d870088ae9d80a5b6
Chuck Taylor <cpt2948@gmail.com>
2025-06-18 23:14:16 -0400

    Implement edit button

--- project 1/GUI.py
+++ project 1/GUI.py
@@ -5,10 +5,13 @@ import FreeSimpleGUI as gui
 label=gui.Text("Type in a to-do")
 input_box = gui.InputText(tooltip = "Enter to-do", key="todo")
 add_button = gui.Button("Add")
+list_box = gui.Listbox(values= function.get_todos(), key='todos',
+                       enable_events=True, size=[45,10])
+edit_button = gui.Button("Edit")
 
 #create a window and button
 window = gui.Window('My To-Do App', 
-                    layout=[[label], [input_box,add_button]], 
+                    layout=[[label], [input_box,add_button],[list_box, edit_button]], 
                     font=('Helvetica',15))
 
 while True:
@@ -16,6 +19,7 @@ while True:
     event,values = window.read()
     print(event)
     print(values)
+    #print(values['todos'])
     
     match event:
         case "Add":
@@ -23,6 +27,23 @@ while True:
             new_todo = values['todo']+"\n"
             todos.append(new_todo)
             function.write_todos(todos)
+            window['todos'].update(values=todos)
+        
+        
+        case "Edit":
+            todo_to_edit = values['todos'][0]
+            new_todo = values['todo']
+           
+            todos = function.get_todos()
+            index= todos.index(todo_to_edit)
+            todos[index]= new_todo
+            function.write_todos(todos)
+            window['todos'].update(values=todos)
+        
+        case "todos":
+            window['todos'].Update(value= values['todos'][0])
+            
+        # error of none that can kill your program
         case gui.WIN_CLOSED:
             break
     
@@ -30,4 +51,4 @@ while True:
     
     
     
-    window.close()
+window.close()
