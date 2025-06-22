10a200b8cc986efa7fda2b126079d74bbdad641a
Chuck Taylor <cpt2948@gmail.com>
2025-06-22 13:10:15 -0400

    Add zip function file

--- small program/zip file creater.py
+++ small program/zip file creater.py
@@ -1,18 +1,32 @@
 #
 import FreeSimpleGUI as gui
+from zip_function import make_archive
 
-
-label = gui.Text("Select files to compress")
+# files to compress
+label = gui.Text("Select files to compress:")
 input1= gui.Input()
-choose_button1 = gui.FileBrowse("Choose")
+choose_button1 = gui.FileBrowse("Choose", key='files')
 
-label2 = gui.Text("Select files to compress")
+#folder to save file
+label2 = gui.Text("Select folder save to:")
 input2= gui.Input()
-choose_button2 = gui.FolderBrowse("Choose")
+choose_button2 = gui.FolderBrowse("Choose", key='folder')
 
 compress_button =gui.Button("Compress")
+output_label = gui.Text(key="output")
 
-window = gui.Window("File Compressor",layout=[[label, input1, choose_button1],[label2, input2, choose_button2],[compress_button]])
+window = gui.Window("File Compressor",layout=[[label, input1, choose_button1],
+                                              [label2, input2, choose_button2],
+                                              [compress_button,output_label]])
 
+while True:
+    event,values = window.read()
+    print(event,values)
+    filepaths= values['files'].split(';')
+    folder = values["folder"]
+    make_archive(filepaths, folder)
+    window["output"].update(value="Completed")
+    
+    
 window.read()
 window.close()
---
+++ small program/zip_function.py
@@ -0,0 +1,15 @@
+import zipfile
+import pathlib
+
+def make_archive(filepaths, dest_dir):
+    dest_path = pathlib.Path(dest_dir,"compress.zip")
+    with zipfile.ZipFile(dest_path,'w') as archive:
+        
+        for filepath in filepaths:
+            filepath = pathlib.Path(filepath)
+            archive.write(filepath, arcname=filepath.name)
+            
+            
+# test function
+if __name__ == '__main__':
+    make_archive(filepaths=["RPS.py","mad-libs.py"], dest_dir= "test_zone")
