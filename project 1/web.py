e39801f4763cd143131069330dc82737716a56cd
Chuck Taylor <cpt2948@gmail.com>
2025-07-01 22:17:43 -0400

    Added remove function to web app

--- project 1/web.py
+++ project 1/web.py
@@ -5,18 +5,23 @@ import function
 todos= function.get_todos()
 
 def add_todo():
-    todo=st.session_state["new_todo"]
+    todo=st.session_state["new_todo"]+"\n"
     todos.append(todo)
     function.write_todos(todos)
 
 todos = function.get_todos()
 
 st.title("My Todo App")
-st.subheader("test")
+st.subheader("My todo list")
 st.write("This app to have you be more productive.")
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        function.write_todos(todos)
+        del st.session_state[todo]
+        st.rerun()
     
 st.text_input(label="", 
               placeholder= "Add todo..", 
