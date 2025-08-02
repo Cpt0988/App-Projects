4b40604033b12b43e5ba4e34bfe2ab9f0f932453
Chuck Taylor <cpt2948@gmail.com>
2025-08-02 13:19:01 -0400

    Added dropdown to contact page

--- Company website/pages/Contact Us.py
+++ Company website/pages/Contact Us.py
@@ -1,15 +1,17 @@
 import streamlit as st
-from send_email import send_email
+from send_email import send_email 
 
 st.header("Contact Us")
 
 with st.form(key = "email form"):
+    selection =st.selectbox("What topic do you want to discuss",
+                            ("Job Inquiries","Project Proposals","Other"))
     user_email = st.text_input("Your Email Address")
     raw_message = st.text_area("Your message")
     message = f"""\
 Subject: New email from {user_email}
 
-From:{user_email}
+From:{selection}
 {raw_message}
     """
     button= st.form_submit_button("Submit")
