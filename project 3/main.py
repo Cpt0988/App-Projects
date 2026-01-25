6b1fc28c4ae1308d17b565dce141dad09e95903c
Chuck Taylor <cpt2948@gmail.com>
2026-01-25 15:46:40 -0500

    send email with article titles

--- project 3/main.py
+++ project 3/main.py
@@ -1,12 +1,21 @@
 import requests #get the contest of the webpage
+from send_email import send_email
+
 
 apiKey="c3daa79d998c426490672da8c02bdf05"
 url = "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=c3daa79d998c426490672da8c02bdf05"
 
-#make a request, get dictionary with data & access article
+#make a request, get dictionary with data & access article title and description
 request = requests.get(url)
 content = request.json()
-
+body =""
 for article in content["articles"]:
-    print(article["title"])
-    print(article["description"])
+    if article["title"]is not None:
+        
+        body = body + article["title"] + "\n" +article["description"] +2*"\n"
+body.encode("utf-8")   
+send_email(message=body)
+    
+    
+    #print(article["title"])
+    #print(article["description"])
