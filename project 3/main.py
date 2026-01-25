cde78227712d0bdd190bc493d7e945d3144a2d60
Chuck Taylor <cpt2948@gmail.com>
2026-01-25 15:11:17 -0500

    access news title and descriptions

--- project 3/main.py
+++ project 3/main.py
@@ -3,7 +3,10 @@ import requests #get the contest of the webpage
 apiKey="c3daa79d998c426490672da8c02bdf05"
 url = "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=c3daa79d998c426490672da8c02bdf05"
 
-#get the contest
+#make a request, get dictionary with data & access article
 request = requests.get(url)
-content = request.text
-#print(content)
+content = request.json()
+
+for article in content["articles"]:
+    print(article["title"])
+    print(article["description"])
