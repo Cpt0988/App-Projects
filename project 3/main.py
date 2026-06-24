87b8ed1d78dbf674958ca101d14bb8b06696f9c8
Chuck Taylor <cpt2948@gmail.com>
2026-06-23 22:21:14 -0400

    updated URL

--- project 3/main.py
+++ project 3/main.py
@@ -3,7 +3,7 @@ from send_email import send_email
 
 
 apiKey="c3daa79d998c426490672da8c02bdf05"
-url = "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=c3daa79d998c426490672da8c02bdf05"
+url = "https://newsapi.org/v2/everything?q=tesla&from=2026-05-24&sortBy=publishedAt&apiKey=c3daa79d998c426490672da8c02bdf05"
 
 #make a request, get dictionary with data & access article title and description
 request = requests.get(url)
@@ -11,7 +11,7 @@ content = request.json()
 body =""
 for article in content["articles"]:
     if article["title"]is not None:
-        body = body + article["title"] + "\n" + article["description"] + 2*"\n"
+        body = body + article["title"] + "\n"+ article["description"] + 2*"\n"
 body=body.encode("utf-8")   
 send_email(message=body)
     
