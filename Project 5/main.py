0ce760ca702e3bc56eb8c06d4daaf33aba6b6588
Chuck Taylor <cpt2948@gmail.com>
2026-07-03 23:42:11 -0400

    Clean up Main file and update the model

--- Project 5/main.py
+++ Project 5/main.py
@@ -1,5 +1,5 @@
 
-# from langchain_google_genai import ChatGoogleGenerativeAI
+
 
 import os
 import uuid
@@ -11,20 +11,22 @@ from langchain.agents import create_agent
 from langchain_ollama import ChatOllama
 from datetime import datetime
 from langgraph.checkpoint.sqlite import SqliteSaver
+from langchain_community.tools.tavily_search import TavilySearchResults
 
 
 load_dotenv() 
 
-# google_api_key = os.getenv("Google_api_key")
 
-conn = sqlite3.connect("chat_history.db",check_same_thread=False)
+
+conn = sqlite3.connect("chat_history.db",
+                       check_same_thread=False)
 checkpointer = SqliteSaver(conn)
+search_tool = TavilySearchResults()
 
 def get_date():
     """Get the current date"""
     return datetime.now().strftime("%Y-%m-%d")
 
-# llm = ChatGoogleGenerativeAI(model ="gemini-3-flash-preview", api_key=google_api_key)
 llm = ChatOllama(model="qwen2.5:3b")
 
 # Mission statement: Create an AI agent that can answer questions and provide information to users. 
@@ -32,20 +34,19 @@ llm = ChatOllama(model="qwen2.5:3b")
 system_prompt = """
 You are a helpful assistant.
 Answer all user questions to the best of your ability. 
-Use the get_date tool if they ask about today's date.
+Use the get_date tool only when the user asks about today's date.
+Use the search_tool for answering questions that require up-to-date information.
+
 
 """
 
-agent = create_agent(model=llm, tools=[get_date], 
+agent = create_agent(model=llm,
+                     tools=[get_date, search_tool], 
                      system_prompt=system_prompt,
                      checkpointer=checkpointer)
 
 
-# user_input = input("What is your question? ")
-# response = agent.invoke({"messages": [{"role": "user", "content": "Hi there!"}]})
 
-# print(response["messages"][-1].content[0]["text"])
-# print(response['messages'][-1].content)
 
 def chat(message, history, thread_id):
     config = {"configurable" : {"thread_id": thread_id}}
